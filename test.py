144f05672dc99eb3d5afbfda544e5a4cde310469
My Name <85300097+dickleung0301@users.noreply.github.com>
2024-08-14 17:53:47 +0200

    delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from load_dataset import *
-
-data = load_flores200_few_shot_in_context('dev', 'devtest', 'eng_Latn', 'deu_Latn', "English: ", "German: ", 3)
-print(data['src'][26])
-print(data['eng_Latn'][26])
-print(data['deu_Latn'][26])
