1cdc0c298218f3160181c2d6e707c511f985e5e1
My Name <85300097+dickleung0301@users.noreply.github.com>
2024-08-16 17:42:04 +0200

    updated comet score

--- comet.py
+++ comet.py
@@ -1,7 +1,12 @@
 import json
 from comet import download_model, load_from_checkpoint
 
-data_path = './zero_shot_result/eng_Latn2gle_Latn_comet'
+file_dir = './zero_shot_result/'
+source_lang = 'eng_Latn'
+target_lang = 'gle_Latn'
+
+data_path = file_dir + source_lang + '2' + target_lang + '_comet'
+save_path = file_dir + source_lang + '2' + target_lang + '.comet'
 
 model_path = download_model("Unbabel/wmt22-comet-da")
 
@@ -18,3 +23,6 @@ with open(data_path, 'r') as file:
 
 model_output = model.predict(data_list, batch_size=32, gpus=1)
 print(model_output.system_score)
+
+with open(save_path, 'w') as f:
+  f.write(f'COMET Score: {model_output.system_score}\n')
--- notes.md
+++
@@ -1,2 +0,0 @@
-look into the details of scareBLUE score (the mathematics formulation)
-look into the details of SentencePiece
