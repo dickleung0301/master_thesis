082bdc93a6014c87d67e68da690bdd8c1331cb12
My Name <85300097+dickleung0301@users.noreply.github.com>
2024-08-29 14:47:33 +0200

    added add experiments

--- corpus_bleu.py
+++ corpus_bleu.py
@@ -3,9 +3,9 @@ import os
 
 # get the current and file directory
 cwd = os.getcwd()
-file_dir = '/few_shot_in_context_result/'
-source_lang = 'fra_Latn'
-target_lang = 'eng_Latn'
+file_dir = '/zero_shot_result/english_to_french/'
+source_lang = 'eng_Latn'
+target_lang = 'fra_Latn'
 trans_path = cwd + file_dir + source_lang +'2' + target_lang + '_trans.txt'
 trg_path = cwd + file_dir + source_lang +'2' + target_lang + '_trg.txt'
 save_path = cwd + file_dir + source_lang + '2' + target_lang + '.bleu'
